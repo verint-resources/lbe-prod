8fcf0ec9fe14c94108ff27fd135b1c7eef9a40ff
ver-dev-workings <69791497+ver-dev-workings@users.noreply.github.com>
2020-08-17 17:18:16 +0100

    Update manageindividual.js

--- manageindividual.js
+++ manageindividual.js
@@ -3,7 +3,7 @@
 var individualTemplateIdentifier = 'individual_template_';
 
 function do_KDF_Ready_Individual(event, kdf){
-	
+	console.log('do_KDF_Ready_Individual');
 if (KDF.getVal('txt_customer_id') !== '' && KDF.getVal('rad_viewmode') !== 'R' && KDF.getVal('rad_viewmode') !== 'U') {
 	    KDF.customdata('person-retrieve-new', 'individual_template_KDF_Ready', true, true, {'person_search_results': KDF.getVal('txt_customer_id')});
 	}
