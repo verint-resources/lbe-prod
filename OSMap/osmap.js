f8a37d2fd9ba932537b8692554af198534c407f5
ver-dev-workings <69791497+ver-dev-workings@users.noreply.github.com>
2020-09-29 10:47:34 +0700

    Update osmap.js

--- OSMap/osmap.js
+++ OSMap/osmap.js
@@ -10,7 +10,7 @@ proj4.defs([
 		]
 	]);
 
-function do_KDF_fieldChange_OSMap(event,kdf,currentpageid,targetpageid) {
+function do_KDF_pageChange_OSMap(event,kdf,currentpageid,targetpageid) {
 	$('.dform_page[data-pos="'+targetpageid+'"] div[id="map"]').each(function() {
 		initialiseOSMap(this);
 	});
