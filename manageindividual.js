d7f5e2a49e4aaabb9c371b9a28a1d1ff2aec55a1
ver-dev-workings <69791497+ver-dev-workings@users.noreply.github.com>
2020-11-05 10:37:10 +0700

    Update manageindividual.js

--- manageindividual.js
+++ manageindividual.js
@@ -4,11 +4,13 @@ var individualTemplateIdentifier = 'individual_template_';
 
 function do_KDF_Ready_Individual(event, kdf) {
     console.log('do_KDF_Ready_Individual');
-	
+    
+	/* Not required for indivudal template to call person-retrieve-enew, information populated via Profile
     if (KDF.getVal('txt_customer_id') !== '' && KDF.getVal('rad_viewmode') !== 'R' && KDF.getVal('rad_viewmode') !== 'U') {
         KDF.customdata('person-retrieve-new', individualTemplateIdentifier + 'KDF_Ready', true, true, { 'person_search_results': KDF.getVal('txt_customer_id') });
     }
-    else if (KDF.kdf().access === 'citizen') {
+    else */ 
+    if (KDF.kdf().access === 'citizen') {
     	KDF.showSection('area_customer_information');
 
 	    KDF.showWidget('ps_citizen_property_search');
