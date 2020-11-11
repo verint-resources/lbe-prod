7df307d360d27179d3e9a43799ad1ebad44b57a7
ver-dev-workings <69791497+ver-dev-workings@users.noreply.github.com>
2020-11-11 17:22:37 +0700

    Update style-4.js

--- style-4.js
+++ style-4.js
@@ -66,10 +66,8 @@ function commonRegex() {
 	regexSearch('[0-9A-Za-z ]{2,}', '.dform_widget_searchfield.txt-gov [data-customalias="name"]');
     regexSearch('[0-9A-Za-z ]{1,}',
         '.dform_widget_searchfield.txt-gov [data-customalias="forename"]');
-    regexSearch('^(EN|en|eN|En|N|n)[A-Za-z0-9 \\s]{0,6}$',
-        '[name="txt_postcode"] input');
-    regexSearch('^(EN|en|eN|En|N|n)[A-Za-z0-9 \\s]{0,6}$',
-        '.dform_widget_searchfield.txt-gov [data-customalias="postcode"]');//KS: Fikri to provide a more comprehensive version
+    //regexSearch('^(EN|en|eN|En|N|n)[A-Za-z0-9 \\s]{0,6}$', '[name="txt_postcode"] input');
+    //regexSearch('^(EN|en|eN|En|N|n)[A-Za-z0-9 \\s]{0,6}$', '.dform_widget_searchfield.txt-gov [data-customalias="postcode"]');//KS: Fikri to provide a more comprehensive version
 
 	/*/KS: to quickly chnage the create new individual validation
 	regexSearch('^([0-9A-Za-z ]{0,})(\S{1}$)',
