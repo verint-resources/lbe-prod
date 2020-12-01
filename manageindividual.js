c343fb89da6420c2733130f5c1fca81e2d1720de
ver-dev-workings <69791497+ver-dev-workings@users.noreply.github.com>
2020-12-01 15:20:43 +0700

    Update manageindividual.js

--- manageindividual.js
+++ manageindividual.js
@@ -9,7 +9,7 @@ function do_KDF_Ready_Individual(event, kdf) {
     addAccordion();
     
 	/* Enable only for test, Please disable after you test - Not required for indivudal template to call person-retrieve-enew, information populated via Profile
-    */if (KDF.getParams().customerid !== '' && KDF.getVal('rad_viewmode') !== 'R' && KDF.getVal('rad_viewmode') !== 'U') {
+    */if (typeof KDF.getParams().customerid !== 'undefined' && KDF.getParams().customerid !== '' && KDF.getVal('rad_viewmode') !== 'R' && KDF.getVal('rad_viewmode') !== 'U') {
         KDF.customdata('person-retrieve-new', individualTemplateIdentifier + 'KDF_Ready', true, true, { 'person_search_results': KDF.getParams().customerid });
     }
 	
