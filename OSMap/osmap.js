ae6b7ce6be3ab0db32fd08b4f52b1f48d92e45cc
elduinn <luthfan.bumi@gmail.com>
2021-02-09 10:49:09 +0700

    Update osmap.js

--- OSMap/osmap.js
+++ OSMap/osmap.js
@@ -1,7 +1,7 @@
 var s = document.createElement("script");
 s.type = "text/javascript";
 s.src = "https://cdn.jsdelivr.net/npm/@turf/turf@6.3.0/turf.min.js";
-$("head").append(s);
+document.head.appendChild(s);
 
 var map, pinMarker, openCasesMarkers;
 var osmapTemplateIdentifier = 'osmap_template_';
