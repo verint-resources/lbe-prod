048465437968309871b7fcd15cc53512d98af78a
elduinn <luthfan.bumi@gmail.com>
2021-03-01 16:26:36 +0700

    Update osmap.js

--- OSMap/osmap.js
+++ OSMap/osmap.js
@@ -450,7 +450,7 @@ function findNearest(point, features) {
 	} else if (nearestFeature.properties.Descriptor1 !==''){
 		streetName = nearestFeature.properties.Descriptor1 + ', ' + nearestFeature.properties.Town1;
 	} else {
-		streetName = 'Your location has been noted.'
+		streetName = nearestFeature.properties.NationalRoadCode + ', ' + nearestFeature.properties.Town1;
 	}
 
 	var popupContent = streetName;
