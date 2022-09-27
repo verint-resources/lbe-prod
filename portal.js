9e6cb40df364aa5e5835a3f039531a5c63381f54
Bilal Ahmed Abbas <84194687+bilibilal1@users.noreply.github.com>
2022-09-27 15:16:17 +0100

    aria-label fix

--- portal.js
+++ portal.js
@@ -55,13 +55,13 @@ var _appConfig = {
             search: function () {
                 $(_app.funnelBack.results).addClass("loading"), _app.funnelBack.xhr1 && _app.funnelBack.xhr1.abort(), _app.funnelBack.xhr2 && _app.funnelBack.xhr2.abort(), _app.funnelBack.xhr3 && _app.funnelBack.xhr3.abort();
                 let e = "";
-                (e += '<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-live="polite" aria-labelledby="search-label">'),
+                (e += '<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-live="polite" aria-labelledby="header-search__results">'),
                     (e += '<li id="search-suggested" role="presentation" class="header-search__category header-search__category--full"></li>'),
                     (e += '<li id="search-services" role="presentation" class="header-search__category"></li>'),
                     (e += '<li id="search-news" role="presentation" class="header-search__category"></li>'),
                     (e += "</ul>"),
                     $(_app.funnelBack.results).html(
-                        '<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-live="polite" aria-labelledby="search-label"><li id="search-suggested" role="presentation" class="header-search__category header-search__category--full"></li><li id="search-services" role="presentation" class="header-search__category"></li><li id="search-news" role="presentation" class="header-search__category"></li></ul>'
+                        '<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-live="polite" aria-labelledby="header-search__results"><li id="search-suggested" role="presentation" class="header-search__category header-search__category--full"></li><li id="search-services" role="presentation" class="header-search__category"></li><li id="search-news" role="presentation" class="header-search__category"></li></ul>'
                     ),
                     (_app.funnelBack.xhr1 = $.getJSON(_app.funnelBack.u + "&profile=_default&show=" + _app.funnelBack.max + "&partial_query=" + _app.funnelBack.q, function (e) {
                         let a = '<h3 class="header-search__heading">Suggested</h3>';
