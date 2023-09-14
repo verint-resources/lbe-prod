1431d6b5bd66a37b75d1e1e7b566c0d04a4d4602
Neetu Dave <web.team@enfield.gov.uk>
2023-09-14 09:09:07 +0100

    Update portal.js
    remove aria-live from search

--- portal.js
+++ portal.js
@@ -115,13 +115,13 @@ var _appConfig = {
                     (_app.funnelBack.q = $(this).val()),
                         _app.funnelBack.q.length > 2
                             ? (_app.funnelBack.search(), (_app.funnelBack.current = -1))
-                            : $(_app.funnelBack.results).html('<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-live="polite" aria-label="suggested"></ul>');
+                            : $(_app.funnelBack.results).html('<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-label="suggested"></ul>');
                 }),
                     $(document).on("click focus keyup", function (e) {
                         $(e.target).is(_app.funnelBack.section + " *") ||
                             ($(_app.funnelBack.section)
                                 .find(_app.funnelBack.results)
-                                .html('<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-live="polite" aria-label="suggested"></ul>'),
+                                .html('<ul role="listbox" id="suggested" class="header-search__wrapper header-search__results-list" aria-label="suggested"></ul>'),
                             $(_app.funnelBack.box).val(_app.funnelBack.q).attr("aria-expanded", "false"));
                     }),
                     $(_app.funnelBack.section).on("click", _app.funnelBack.suggestion + " li", function (e) {
