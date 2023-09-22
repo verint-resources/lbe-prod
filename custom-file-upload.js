8ee6e6436f8e45310b7bc2bc10de591706fc544e
Neetu Dave <web.team@enfield.gov.uk>
2023-09-22 11:48:55 +0100

    Update custom-file-upload.js
    add for attribute to file upload label

--- custom-file-upload.js
+++ custom-file-upload.js
@@ -221,7 +221,7 @@ function do_KDF_Custom_Sharepoint(response, action) {
             if ($('#custom_fileupload_holder').length > 0) {
 
                 var widget = '<div data-type="file" data-name="file_ootb" data-active="true" data-agentonly="false" class="file-progress lbe-file-gov">' +
-                    '<div><label>' + sharepoint_title + '</div></label>' +
+                    '<div><label for="custom_fileupload">' + sharepoint_title + '</div></label>' +
                     '<div style="position: relative;"><input id="custom_fileupload" type="file" name="uploadedFile">' +
                     '<span class="file-gov-icon"><span class="file-gov-icon-a"></span><span class="file-gov-icon-b"></span><label class="file-gov-text">Upload file</label></span>' +
                     '<div class="helptext">Image file types accepted are ' + formParams.allowedFileType + ' up to ' + formParams.maxFileSizeDisplay + ' MB in size</div>' +
