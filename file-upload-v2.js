56e89803d414ce2a6319856380a219a231de4012
Neetu Dave <web.team@enfield.gov.uk>
2024-09-12 11:02:04 +0100

    Update file-upload-v2.js

--- file-upload-v2.js
+++ file-upload-v2.js
@@ -20,6 +20,10 @@ function setFiledNames(fieldnames) {
     formParams.fieldNames = fieldnames;
 }
 
+function do_KDF_Ready_Sharepoint(event, kdf) {
+    do_KDF_Ready_SharepointV2(event, kdf);
+}
+
 function do_KDF_Ready_SharepointV2(event, kdf) {
     var hostURL = window.location.href;
     if (hostURL.indexOf("https://lbe.") > -1) {
